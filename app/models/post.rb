e8660d59ba9605742626fd7a8773c7a8f0982024
Asri Nurul Ma'rifah <asrimarifah0402@gmail.com>
2021-12-18 22:38:09 +0700

    post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
-class Community < ApplicationRecord
+class Post < ApplicationRecord
   belongs_to :account
   belongs_to :community
   validates_presence_of :title, :body, :account_id, :community_id
