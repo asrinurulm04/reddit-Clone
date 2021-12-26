9028fd21501f844fb9950115726ca9a06daff04b
Asri Nurul Ma'rifah <asrimarifah0402@gmail.com>
2021-12-26 19:35:36 +0700

    new

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,19 @@ class Vote < ApplicationRecord
   belongs_to :account
   belongs_to :post
   validates_presence_of :account_id, :scope, :post_id
-  has_many  :comments
+
+  after_create :increment_vote
+  after_destroy :decrement_vote
+
+  private
+
+  def increment_vote
+    field = self.upvote ? :upvotes : :downvotes
+    Post.find(self.post_id).increment(field).save
+  end
+
+  def decrement_vote
+    field = self.upvote ? :upvotes : :downvotes
+    Post.find(self.post_id).decrement(field).save
+  end
 end
